3645bdf51fc2a89df7e1a67a6e684123c5c83fe8
Matthew Zhang <70178663+caspinprince@users.noreply.github.com>
2022-01-03 02:24:39 -0500

    refactoring

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 import pymysql
-
+from flask_migrate import Migrate
 login_manager = LoginManager()
 
 app = Flask(__name__)
@@ -12,6 +12,7 @@ app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 login_manager.init_app(app)
 login_manager.login_view = 'login'
--- web_app/models.py
+++ web_app/models.py
@@ -1,6 +1,7 @@
 from app import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -16,6 +17,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True, index=True)
     password_hash = db.Column(db.String(128), nullable=True)
     google_id = db.Column(db.String(64), unique=True, index=True, nullable=True)
+    about_me = db.Column(db.String(1000))
+    last_online = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __init__(self, first_name, last_name, email, username, password=None, google_id=None):
         self.first_name = first_name
