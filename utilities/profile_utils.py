ecc78a33f1d6578d8aa143f1f89366ac57b90e5a
Matthew Zhang <70178663+caspinprince@users.noreply.github.com>
2022-01-12 20:21:09 -0500

    fix last online display bug

--- utilities/profile_utils.py
+++ utilities/profile_utils.py
@@ -3,12 +3,13 @@ from datetime import datetime, timedelta
 def time_diff(timestamp):
     current_time = datetime.utcnow()
     t_delta = (current_time - timestamp).total_seconds()
-    time_conversions = {60:'mins', 3600:'hrs', 86400:'days', 31536000:'years'}
+    time_conversions = {'mins': 60, 'hrs': 60, 'days': 24, 'years': 365}
     t_delta_str = "Online"
     for x in time_conversions:
-        if t_delta/x > 1:
-            t_delta_str = str(int(t_delta//x)) + " " + time_conversions[x] + " ago"
-            t_delta = t_delta/x
+        num = time_conversions[x]
+        if t_delta/num > 1:
+            t_delta_str = str(int(t_delta//num)) + " " + x + " ago"
+            t_delta = t_delta/num
         else:
             break
 
