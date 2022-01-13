2ac79e6b8a493905c861589bfb6d38dd596da4d1
Matthew Zhang <70178663+caspinprince@users.noreply.github.com>
2022-01-12 19:51:15 -0500

    create flask environment variable file and update config file

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+base_dir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(base_dir,'.env'))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    EXPLAIN_TEMPLATE_LOADING = True
